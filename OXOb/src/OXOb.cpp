e23b0b12384bb8e9cefb9998618fc291be2b92bd
J. M. Hinckley <v8vette@gmail.com>
2018-11-25 19:11:19 -0500

    Converted OXO from just drawing the board into a two-player game

--- OXOb/src/OXOb.cpp
+++ OXOb/src/OXOb.cpp
@@ -9,12 +9,9 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	int board[3][3] = {
-			1,2,0,
-			0,2,0,
-			1,2,1};
-
+void DrawBoard(int bd[3][3])
+{
+	// Draw the board, using the input game model bd.
 	// Top and bottom rows of the board.
 	string caprow = "           ||         ||           ";
 
@@ -41,11 +38,11 @@ int main() {
 	for (int i = 0; i < 4; i++)	// looping over rows in sprites
 	{
 		cout << sp;
-		cout << sprite[board[0][0]][i];
+		cout << sprite[bd[0][0]][i];
 		cout << vbar;
-		cout << sprite[board[0][1]][i];
+		cout << sprite[bd[0][1]][i];
 		cout << vbar;
-		cout << sprite[board[0][2]][i];
+		cout << sprite[bd[0][2]][i];
 		cout << sp;
 		cout << endl;
 	}
@@ -56,11 +53,11 @@ int main() {
 	for (int i = 0; i < 4; i++)	// looping over rows in sprites
 	{
 		cout << sp;
-		cout << sprite[board[1][0]][i];
+		cout << sprite[bd[1][0]][i];
 		cout << vbar;
-		cout << sprite[board[1][1]][i];
+		cout << sprite[bd[1][1]][i];
 		cout << vbar;
-		cout << sprite[board[1][2]][i];
+		cout << sprite[bd[1][2]][i];
 		cout << sp;
 		cout << endl;
 	}
@@ -71,16 +68,131 @@ int main() {
 	for (int i = 0; i < 4; i++)	// looping over rows in sprites
 	{
 		cout << sp;
-		cout << sprite[board[2][0]][i];
+		cout << sprite[bd[2][0]][i];
 		cout << vbar;
-		cout << sprite[board[2][1]][i];
+		cout << sprite[bd[2][1]][i];
 		cout << vbar;
-		cout << sprite[board[2][2]][i];
+		cout << sprite[bd[2][2]][i];
 		cout << sp;
 		cout << endl;
 	}
 
 	cout << caprow << endl;	// o/p bottom row
+
+}
+
+
+
+bool CheckWin(int bd[3][3], int player)
+{
+	bool playerWins = false;
+
+	// Check the rows for three occurrences of player
+	// If found, set playerWins = true
+	for (int row = 0; row < 3; row++)
+	{
+		if (bd[row][0]==player && bd[row][1]==player && bd[row][2]==player)
+		{
+			playerWins = true;
+			break;
+		}
+	}
+
+
+	// If playerWins == false, then check the columns for three
+	// occurrences of player.
+	// If found, set playerWins = true
+	if (!playerWins)
+	{
+		// add code here...
+	}
+
+
+
+	// If playerWins == false, then check the NW-SE diagonal for
+	// three occurrences of player.
+	// If found, set playerWins = true
+	if (!playerWins)
+	{
+		// add code here...
+	}
+
+
+
+	// If playerWins == false, then check the NE-SW diagonal for
+	// three occurrences of player.
+	// If found, set playerWins = true
+	if (!playerWins)
+	{
+		// add code here...
+	}
+
+
+	return playerWins;
+}
+
+
+
+int main() {
+	bool xWins = false, oWins = false;
+
+
+	// initialize the game model
+	int board[3][3] = {
+			0,0,0,
+			0,0,0,
+			0,0,0};
+
+	// draw the board
+	DrawBoard(board);
+
+	for (int turn = 0; turn < 9; turn++)
+	{
+		if (turn % 2 == 0)
+		{
+			// X's turn
+
+			// Ask for row, column positions for X's move
+			// Keep asking while row < 0 or row > 2 or column < 0 or column > 2 or
+			// board[row][column] != 0.
+			// Use a do-while loop for this.
+			// Add code here...
+
+
+
+			// Update the model.
+			// board[row][column] = 1
+			// Add code here...
+
+
+			// Redraw the board with the new move.
+			DrawBoard(board);
+
+
+			// Check whether X won.
+			xWins = CheckWin(board, 1);
+
+
+			// if X wins, announce it and leave the loop
+			if (xWins)
+			{
+				cout << "X wins" << endl;
+				break;
+			}
+		}
+		else
+		{
+			// O's turn
+
+			// Add code here...
+		}
+	}
+
+
+	if (!xWins && !oWins)
+	{
+		cout << "Draw" << endl;
+	}
 
 
 	return 0;
